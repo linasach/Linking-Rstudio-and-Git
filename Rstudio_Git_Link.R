65ebe3832eaa2f0cc9245ce0273402ffc88f5a6c
Idalina Sachango <isd2@rice.edu>
2020-07-13 13:34:22 -0500

    Calculated the mean of Sepal.Length.

--- Rstudio_Git_Link.R
+++ Rstudio_Git_Link.R
@@ -1,2 +1,9 @@
 print("This file was created within RStudio")
 print("And now it lives on Github")
+
+library(dplyr)
+
+iris_data <- iris
+iris_data %>% 
+  dplyr::group_by(Species) %>% 
+  dplyr::summarise(mean = mean(Sepal.Length))
