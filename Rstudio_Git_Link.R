f2723eb76f8116e9e7f77203acada5eae47aecfe
Idalina Sachango <isd2@rice.edu>
2020-07-13 13:42:18 -0500

    Created a new branch called "construction." Commited a ggplot script to this new branch.

--- Rstudio_Git_Link.R
+++ Rstudio_Git_Link.R
@@ -2,8 +2,12 @@ print("This file was created within RStudio")
 print("And now it lives on Github")
 
 library(dplyr)
+library(ggplot2)
 
-iris_data <- iris
-iris_data %>% 
+iris_data <- iris %>% 
   dplyr::group_by(Species) %>% 
   dplyr::summarise(mean = mean(Sepal.Length))
+
+ggplot(data = iris_data, aes(x = Species, y = mean)) +
+  geom_point(stat = "identity") +
+  scale_y_continuous(limits = c(4,7), breaks = c(4, 4.5, 5, 5.5, 6, 6.5, 7))
